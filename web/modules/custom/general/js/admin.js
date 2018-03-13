4a92a44e5089db1f5298602624494b4af64f99e9
John Burch <JBurch@acas.org.uk>
2018-03-13 20:46:42 +0000

    hook path auto modify

--- web/modules/custom/general/js/admin.js
+++ web/modules/custom/general/js/admin.js
@@ -2,20 +2,20 @@
   Drupal.behaviors.adminBehavior = {
     attach: function (context, settings) {
       $("#node-landing-page-edit-form #edit-field-taxonomy").change(function() {
-        var title = $("option:selected", this).text().replace("-", "");
+        var title = $("option:selected", this).text().replace(/-/g, "");
         $("#node-landing-page-edit-form #edit-title-0-value").val(title);
       });
       $("#node-landing-page-form #edit-field-taxonomy").change(function() {
-        var title = $("option:selected", this).text().replace("-", "");
+        var title = $("option:selected", this).text().replace(/-/g, "");
         $("#node-landing-page-form #edit-title-0-value").val(title);
       });
       
       $("#node-details-page-edit-form #edit-field-taxonomy").change(function() {
-        var title = $("option:selected", this).text().replace("-", "");
+        var title = $("option:selected", this).text().replace(/-/g, "");
         $("#node-details-page-edit-form #edit-title-0-value").val(title);
       });
       $("#node-details-page-form #edit-field-taxonomy").change(function() {
-        var title = $("option:selected", this).text().replace("-", "");
+        var title = $("option:selected", this).text().replace(/-/g, "");
         $("#node-details-page-form #edit-title-0-value").val(title);
       });
     }
