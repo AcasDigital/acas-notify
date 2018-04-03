5c2ccc27174ef2b0f31dbd66cea371ba7034acab
John Burch <JBurch@acas.org.uk>
2018-04-03 15:08:38 +0000

    First commit

---
+++ web/modules/custom/general/src/Form/AdminForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\general\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements the Admin form.
+ */
+class AdminForm extends ConfigFormBase {
+    
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'admin_form';
+  }
+  
+  /** 
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'acas.settings',
+    ];
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('acas.settings');
+    $form['feedback_email'] = array(
+      '#type' => 'textfield',
+      '#default_value' => $config->get('contact_email') ?: 'john@johnburch.co.uk',
+      '#title' => t('Feedback email'),
+      '#description' => t('Alerts will be sent to this address when a user completes the feedback form or clicks "Was this page usefull?"'),
+      '#size' => 100,
+    );
+    return parent::buildForm($form, $form_state);
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::configFactory()->getEditable('acas.settings')
+    ->set('feedback_email', $form_state->getValue('feedback_email'))
+    ->save();
+    parent::submitForm($form, $form_state);
+  }
+}
--- web/themes/custom/acas/src/js/acas.js
+++ web/themes/custom/acas/src/js/acas.js
@@ -13,11 +13,55 @@ Drupal.behaviors.acas = {
   }
 };
 
-function mainNavigationClasses() {
-  var x = document.getElementById("nav-main__topmenu");
-  if (x.className === "nav-main__topmenu") {
-    x.className += " nav-main__responsive";
-  } else {
-    x.className = "nav-main__topmenu";
+
+var dropdownButtons = (function() {
+  // dom elements
+  var primaryWrappers = document.getElementsByClassName('menu-primary__item');
+  var primaryLinks = 'menu-primary__link';
+  var secondaryWrappers = document.getElementsByClassName('menu-secondary__item');
+  var secondaryLinks = 'menu-secondary__link';
+
+  var activeDropdowns = [];
+
+  addEventListeners(primaryWrappers, primaryLinks);
+  addEventListeners(secondaryWrappers, secondaryLinks);
+
+  function addEventListeners(wrappers, linkClass) {
+    // Add click listeners to all the dropdown buttons
+    for (i = 0; i < wrappers.length; i++) {
+      if (wrappers[i].classList.contains('active')) {
+        activeDropdowns.push(wrappers[i]);
+      }
+      for (c = 0; c < wrappers[i].getElementsByClassName(linkClass).length; c++) {
+        wrappers[i].getElementsByClassName(linkClass)[c].addEventListener("click", function() {
+          toggleDropdown(this, event);
+        });
+      }
+    }
   }
-}
+
+  // Hide all other dropdowns
+  function hideDropdowns(activeDropdowns) {
+    for (i = 0; i < activeDropdowns.length; i++) {
+      activeDropdowns[i].classList.remove('active');
+    }
+  }
+
+  // Toggle the dropdown
+  function toggleDropdown(link, event) {
+    event.preventDefault(event);
+    link.parentNode.classList.toggle('active');
+    for (i = 0; i < activeDropdowns.length; i++) {
+      if (activeDropdowns[i] !== this.parentNode) {
+        activeDropdowns[i].classList.remove('active');
+      }
+    }
+  }
+
+  return {
+    // Returning hideDropdowns in case another module needs to access this
+    activeDropdowns: activeDropdowns,
+    hideDropdowns: hideDropdowns
+  }
+
+})();
