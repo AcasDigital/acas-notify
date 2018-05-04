2a292d68ec616e6a1733204cdb24380dc86c8922
John Burch <JBurch@acas.org.uk>
2018-05-04 12:07:25 +0000

    prod test 7

--- web/modules/custom/general/src/Form/SyncForm.php
+++ web/modules/custom/general/src/Form/SyncForm.php
@@ -36,7 +36,7 @@ class SyncForm extends ConfigFormBase {
     }
     $config = $this->config('acas.settings');
     $form['#prefix'] = '<h2>Syncronise content to Production</h2>';
-    $form['#action'] = '/sync-prod';
+    $form['#action'] = '/admin/config/development/sync-prod';
     $form['#attached']['library'][] = 'general/sync_prod';
     $form['#attributes']['onsubmit'] = 'return syncProd()';
     $form['#suffix'] = '<div id="sync_progress" class="hidden">Sync to Production has started, this might take several minutes. Please wait...</div>';
