4314e840560ce7794d1feee7feefa1e56a93546d
John Burch <JBurch@acas.org.uk>
2018-05-12 11:36:32 +0000

    git pull 4

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/html
 FILE="/var/log/git_pull.log"
-cmd_output=$(sudo -u ubuntu /usr/bin/git pull origin master 2>&1)
+cmd_output=$(/usr/bin/git pull origin master 2>&1)
 echo $cmd_output >> $FILE
