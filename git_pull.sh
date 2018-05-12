12feb13f4f77c5c040a04549acb458c29cfa8da7
John Burch <JBurch@acas.org.uk>
2018-05-12 12:32:37 +0000

    git pull 5

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /var/www/html
 FILE="/var/log/git_pull.log"
-cmd_output=$(/usr/bin/git pull origin master 2>&1)
+cmd_output=$(sudo -u ubuntu /usr/bin/git pull origin master 2>&1)
+echo $cmd_output >> $FILE
+cmd_output=$(sudo -u ubuntu /usr/local/bin/composer update 2>&1)
 echo $cmd_output >> $FILE
