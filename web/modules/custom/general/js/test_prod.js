ad7343351243e53055406ca73117a12b5c2ea861
John Burch <JBurch@acas.org.uk>
2018-05-12 10:59:31 +0000

    test 3

--- web/modules/custom/general/js/test_prod.js
+++ web/modules/custom/general/js/test_prod.js
@@ -49,15 +49,14 @@ function getPage(node) {
       jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span></div>');
     },
     success: function(data){
-      jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;');
       var a1 = data.split('last-changed="');
       var a2 = a1[1].split('">');
       var b1 = node.html.split('last-changed="');
       var b2 = b1[1].split('">');
       if (a2[0] == b2[0]) {
-        jQuery("#test-target").html(jQuery("#test-target").html() + '<span class="green">OK</span></div>');
+        jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="green">OK</span></div>');
       }else{
-        jQuery("#test-target").html(jQuery("#test-target").html() + '<span class="red">BAD</span></div>');
+        jQuery("#test-target").html(jQuery("#test-target").html() + '<div class="result"><span class="title">' + node.title + '</span>&nbsp;<span class="red">BAD</span></div>');
       }
       i++
       if (i < nodes.length) {
