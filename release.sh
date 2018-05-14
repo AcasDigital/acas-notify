f92589e610d67f136a0b76f4c7a053e821f0884f
John Burch <JBurch@acas.org.uk>
2018-05-14 10:10:06 +0000

    release script

--- release.sh
+++ release.sh
@@ -13,12 +13,14 @@ fi
 cmd_output=$(/usr/bin/git add . 2>&1)
 cmd_output=$(/usr/bin/git commit -m "$msg" 2>&1)
 if [[ $cmd_output = *"nothing to commit"* ]]; then
-	echo -e "Nothing to commit, working directory clean\n"
+	echo -e "Nothing to commit, working directory clean"
 	exit 1
 fi
-echo -e $cmd_output\n
+echo -e $cmd_output
+echo -e "\n"
 cmd_output=$(/usr/bin/git push origin master 2>&1)
-echo -e $cmd_output\n
+echo -e $cmd_output
+echo -e "\n"
 cmd_output=$(/usr/bin/ssh -i /home/ubuntu/Acas-dev.pem ubuntu@34.243.107.7 'cd /var/www/html; git pull origin master; composer update' 2>&1)
-echo -e $cmd_output\n
-echo -e "Finished release to UAT\n"
+echo -e $cmd_output
+echo -e "\nFinished release to UAT"
