02b62a9ef52311d235e2ec9e368db86a79781f52
John Burch <JBurch@acas.org.uk>
2018-06-10 09:32:37 +0000

    release.sh

--- git_pull.sh
+++ git_pull.sh
@@ -4,8 +4,8 @@ FILE="/var/log/git_pull.log"
 cmd_output=$(sudo -u ubuntu /usr/bin/git pull origin master 2>&1)
 echo $cmd_output >> $FILE
 if [[ $cmd_output = *"Already up-to-date"* ]]; then
-  echo "0"
-  exit
+	echo "0"
+	exit
 fi
 cmd_output=$(sudo -u ubuntu /usr/local/bin/composer update 2>&1)
 echo $cmd_output >> $FILE
